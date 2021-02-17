eff6f4f15d795515e1eb778dd12db3575c97b50a
Travis Deshotels <travis.deshotels@gmail.com>
2021-02-16 22:14:36 -0600

    Update

--- src/fn-test/java/tk/codedojo/bracketgenerator/features/Commandline.feature
+++ src/fn-test/java/tk/codedojo/bracketgenerator/features/Commandline.feature
@@ -6,3 +6,5 @@ Feature: Command Line Test
       | Division | Type | Players              |
       | A        | SE   | Un,Deux,Trois,Quatre |
       | B        | SE   | Cinq,Six,Sept        |
+    When I run the script in the command line
+    And I read the output file
--- src/fn-test/java/tk/codedojo/bracketgenerator/steps/FunctionalSteps.java
+++ src/fn-test/java/tk/codedojo/bracketgenerator/steps/FunctionalSteps.java
@@ -2,7 +2,9 @@ package tk.codedojo.bracketgenerator.steps;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -10,22 +12,45 @@ import java.io.FileWriter;
 import java.util.List;
 
 public class FunctionalSteps {
+    private static final String CSV_FILE = "csvout";
+    private static final String SCRIPT_OUTPUT_FILE = "jsonout";
+    private static final String JAR_FILE = "BracketGeneratorJava-1.0-shaded.jar";
+
     @After
     public void cleanup(){
-        File clean = new File("out");
+        File clean = new File(CSV_FILE);
         if(clean.delete()){
-            System.out.println("Cleanup");
+            System.out.println("Cleanup succeeded");
         }
     }
 
     @Given("I have the following data in my input file")
     public void iHaveTheFollowingDataInMyInputFile(DataTable table) throws Exception {
         List<List<String>> rows = table.asLists(String.class);
-        BufferedWriter writer = new BufferedWriter(new FileWriter("out"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(CSV_FILE));
         for(int i=1;i< rows.size();i++){
             List<String> row = rows.get(i);
             writer.write(row.get(0)+','+row.get(1)+','+row.get(2)+"\n");
         }
         writer.close();
+    }
+
+    @When("I run the script in the command line")
+    public void iRunTheScriptInTheCommandLine() throws Exception {
+        String cmd = "java -jar target/" + JAR_FILE + " " + CSV_FILE + " " + SCRIPT_OUTPUT_FILE;
+        Runtime run = Runtime.getRuntime();
+        Process pr = run.exec(cmd);
+        pr.waitFor();
+        if(pr.exitValue() == 0){
+            System.out.println("Script ran successfully");
+        } else{
+            System.err.println("Script failed");
+            throw new Exception("Script failed to run!");
+        }
+    }
+
+    @And("I read the output file")
+    public void iReadTheOutputFile() {
+
     }
 }
